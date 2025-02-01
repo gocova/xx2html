1692b68b3e47004b240557abb057f3ca446f65cb
gonzalo covarrubias <gocova.dev@gmail.com>
2025-01-31 23:16:20 -0600

    Corrected logger (using logging)

--- src/xx2html/cova/__init__.py
+++ src/xx2html/cova/__init__.py
@@ -1,9 +1,7 @@
 from os import path
 from pathlib import Path
 import json
-
-from kivy.logger import Logger
-
+import logging
 
 def initialize(
     app_config_path: str,
@@ -11,9 +9,9 @@ def initialize(
     default_config: dict,
     system_config: dict,
 ):
-    Logger.info("Application: Initializing...")
+    logging.info("Application: Initializing...")
 
-    Logger.info(f"Application: Using config path |> {app_config_path}")
+    logging.info(f"Application: Using config path |> {app_config_path}")
     Path(app_config_path).mkdir(parents=True, exist_ok=True)
 
     current_config = default_config
@@ -28,5 +26,4 @@ def initialize(
         if isinstance(user_config, dict):
             current_config.update(user_config)
     current_config.update(system_config)
-    Logger.info(f"Application: Current config |> {current_config}")
-
+    logging.info(f"Application: Current config |> {current_config}")
